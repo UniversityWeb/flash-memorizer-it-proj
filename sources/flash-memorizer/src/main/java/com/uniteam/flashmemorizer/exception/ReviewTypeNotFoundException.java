33bcc625cf3d282e44b8c2d4792128580fb87e2a
AnhAnNek <info.vananne@gmail.com>
2023-10-02 17:59:24 +0700

    add custome exception

---
+++ sources/flash-memorizer/src/main/java/com/uniteam/flashmemorizer/exception/ReviewTypeNotFoundException.java
@@ -0,0 +1,5 @@
+package com.uniteam.flashmemorizer.exception;
+
+public class ReviewTypeNotFoundException extends RuntimeException {
+    public ReviewTypeNotFoundException(String message) { super(message); }
+}
